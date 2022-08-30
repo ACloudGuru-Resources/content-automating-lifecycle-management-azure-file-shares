4a7045d5ae9beeceaab84a1f381d181fa8bf1782
BrentenDovey-ACG <68392307+BrentenDovey-ACG@users.noreply.github.com>
2022-08-29 19:54:57 -0500

    Update setup.ps1

--- S01 - L05 - Running Jobs in Azure Container Instances/setup.ps1
+++ S01 - L05 - Running Jobs in Azure Container Instances/setup.ps1
@@ -1,3 +1,7 @@
 # Setup the demo environment
 
-../setup.ps1
+curl https://raw.githubusercontent.com/ACloudGuru-Resources/content-automating-lifecycle-management-azure-file-shares/main/S01%20-%20L05%20-%20Running%20Jobs%20in%20Azure%20Container%20Instances/azuredeploy.json -o azuredeploy.json
+
+az deployment group create `
+--resource-group (az group list --query '[].name' --output tsv) `
+--template-file azuredeploy.json
